399a1b79075043321666c49ec387c1761870b74e
Md. Ibrahim Shipon <156697977+shipon675s@users.noreply.github.com>
2024-11-10 14:22:22 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -2,13 +2,6 @@
 
 // ['', '', '', 1, true] [{}, {}, {}];
 
-const fruits = ['Mango', 'Banana', 'Orange', 'Graps'];
-for(let fruit = 0; fruit < fruits.length; fruit++){
-    // console.log(fruits[fruit]);
-}
-for (const i of fruits) {
-    // console.log(i);    
-}
 
 
 
@@ -30,26 +23,77 @@ country.set('Vu', 'Vutan');
 country.set('Bn', 'Bangladesh');   // Same data not allow
 
 // for (const key of country) {
-//     console.log(key);
-// }
-
-for (const [key, value] of country) {
-    // console.log(key, ':' ,value);
-}
-
-
-
-const myCountry = {
+    //     console.log(key);
+    // }
+    
+    for (const [key, value] of country) {
+        // console.log(key, ':' ,value);
+    }
+    
+    
+    
+    const myCountry = {
     Bn: 'Bangladesh',
     In: 'India',
     Pk: 'Pakistan',
     Np: 'Nepal',
 }
 // for (const key of myCountry) {
-//     // console.log(key, myCountry);     // object is not iterable
-// }
-
-for (const key in myCountry) {
-    console.log(key, ':' ,myCountry[key]);
+    //     // console.log(key, myCountry);     // object is not iterable
+    // }
+    
+    // for (const key in myCountry) {
+        //     console.log(key, ':' ,myCountry[key]);
+        // }
+        
+        for (const key of Object.keys(myCountry)) {
+            // console.log(key, ':', myCountry[key]);     // This rules follow object is iterable
+        }
+        
+        
+        
+const fruits = ['Mango', 'Banana', 'Orange', 'Graps'];
+for(let fruit = 0; fruit < fruits.length; fruit++){
+    // console.log(fruits[fruit]);
+}
+for (const i of fruits) {
+    // console.log(i);    
+}
+for (const key in fruits) {
+    // console.log(key, fruits[key]);
+    // console.log(fruits[key]);
 }
 
+// fruits.forEach(function(fruit) {
+//     console.log(fruit);
+// })
+
+// function myfruit(fruit){
+//     console.log(fruit);
+// }
+// fruits.forEach(myfruit);
+
+fruits.forEach((fruit, index, arr) => {
+    // console.log(fruit, index, arr);
+})
+
+
+
+
+const stack = [
+    {
+        name: 'MERN STSCK',
+        property: 'Mongo, Express, React, Node'
+    },
+    {
+        name: 'Laravel',
+        property: 'JavaScript, vue, PHP, Laravel, MySQL'
+    },
+    {
+        name: 'WordPress',
+        property: 'JavaScript, PHP, WordPress, MySQL'
+    }
+];
+stack.forEach(info => {
+    console.log(`Want to learn ${info.name}? Learn this ${info.property}`);
+});
