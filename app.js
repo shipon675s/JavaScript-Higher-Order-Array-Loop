f0c9ef8346bb1900bcfa7580b1255349d9ad0a97
Md. Ibrahim Shipon <156697977+shipon675s@users.noreply.github.com>
2024-11-11 11:55:48 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -2,62 +2,53 @@
 
 // ['', '', '', 1, true] [{}, {}, {}];
 
-
-
-
-const myName = 'Shipon';
+const myName = "Shipon";
 for (const name of myName) {
     // console.log(`Each char is ${name}`);
 }
 
-
-
 const country = new Map([
-    ['Bn', 'Bangladesh'],
-    ['In', 'India'],
-    ['Pk', 'Pakistan'],
-    ['Np', 'Nepal'],
-])
+    ["Bn", "Bangladesh"],
+    ["In", "India"],
+    ["Pk", "Pakistan"],
+    ["Np", "Nepal"],
+]);
 
-country.set('Vu', 'Vutan');
-country.set('Bn', 'Bangladesh');   // Same data not allow
+country.set("Vu", "Vutan");
+country.set("Bn", "Bangladesh"); // Same data not allow
 
 // for (const key of country) {
-    //     console.log(key);
-    // }
-    
-    for (const [key, value] of country) {
-        // console.log(key, ':' ,value);
-    }
-    
-    
-    
-    const myCountry = {
-    Bn: 'Bangladesh',
-    In: 'India',
-    Pk: 'Pakistan',
-    Np: 'Nepal',
+//     console.log(key);
+// }
+
+for (const [key, value] of country) {
+    // console.log(key, ':' ,value);
 }
+
+const myCountry = {
+    Bn: "Bangladesh",
+    In: "India",
+    Pk: "Pakistan",
+    Np: "Nepal",
+};
 // for (const key of myCountry) {
-    //     // console.log(key, myCountry);     // object is not iterable
-    // }
-    
-    // for (const key in myCountry) {
-        //     console.log(key, ':' ,myCountry[key]);
-        // }
-        
-        for (const key of Object.keys(myCountry)) {
-            // console.log(key, ':', myCountry[key]);     // This rules follow object is iterable
-        }
-        
-        
-        
-const fruits = ['Mango', 'Banana', 'Orange', 'Graps'];
-for(let fruit = 0; fruit < fruits.length; fruit++){
+//     // console.log(key, myCountry);     // object is not iterable
+// }
+
+// for (const key in myCountry) {
+//     console.log(key, ':' ,myCountry[key]);
+// }
+
+for (const key of Object.keys(myCountry)) {
+    // console.log(key, ':', myCountry[key]);     // This rules follow object is iterable
+}
+
+const fruits = ["Mango", "Banana", "Orange", "Graps"];
+for (let fruit = 0; fruit < fruits.length; fruit++) {
     // console.log(fruits[fruit]);
 }
 for (const i of fruits) {
-    // console.log(i);    
+    // console.log(i);
 }
 for (const key in fruits) {
     // console.log(key, fruits[key]);
@@ -75,25 +66,22 @@ for (const key in fruits) {
 
 fruits.forEach((fruit, index, arr) => {
     // console.log(fruit, index, arr);
-})
-
-
-
+});
 
 const stack = [
     {
-        name: 'MERN STSCK',
-        property: 'Mongo, Express, React, Node'
+        name: "MERN STSCK",
+        property: "Mongo, Express, React, Node",
     },
     {
-        name: 'Laravel',
-        property: 'JavaScript, vue, PHP, Laravel, MySQL'
+        name: "Laravel",
+        property: "JavaScript, vue, PHP, Laravel, MySQL",
     },
     {
-        name: 'WordPress',
-        property: 'JavaScript, PHP, WordPress, MySQL'
-    }
+        name: "WordPress",
+        property: "JavaScript, PHP, WordPress, MySQL",
+    },
 ];
-stack.forEach(info => {
+stack.forEach((info) => {
     console.log(`Want to learn ${info.name}? Learn this ${info.property}`);
 });
